e35a1995f4bf81d3bcb44b4d52671d43ce1eddcf
Larissa Cristina Ladeira <larissaladeira42@gmail.com>
2025-12-28 14:31:55 -0300

    adicionei estilos em css

--- ex014/script.js
+++ ex014/script.js
@@ -2,8 +2,8 @@
 function carregar(){
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
-    // var data = new Date()
-    // var hora = data.getHours()
+    var data = new Date()
+    var hora = data.getHours()
 
     var hora = 22
     msg.innerHTML = `Agora são ${hora} horas.`
@@ -11,15 +11,20 @@ function carregar(){
     if (hora >= 0 && hora < 12){
         msg.innerHTML = `Agora são ${hora} horas. Bom dia!`
         img.src = "imagens/foto-manhã.jpg" 
+        document.body.style.background = '#e2cd9f'
 
     }
     else if (hora <= 18 ){
         msg.innerHTML = `Agora são ${hora} horas. Boa tarde!`
         img.src = "imagens/foto-tarde.jpg" 
+        document.body.style.background = '#b9846f'
+
 
     }else{
         msg.innerHTML = `Agora são ${hora} horas. Boa noite!`
-        img.src = "imagens/foto-noite.jpg" 
+        img.src = "imagens/foto-noite.jpg"
+        document.body.style.background = '#515154'
+
 
     }
 }
